4669fca633ad74af2f2524fffd6d956f38778716
Youssef <choukriyoussef7905@gmail.com>
2022-06-16 08:59:57 +0100

    Create the round function

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ function computerPlay() {
     let random = Math.floor(Math.random() * 3 )
     return choice[random]
 }
+
+function round(user_choice, computer_choice) {
+    if(user_choice === computer_choice) {
+        return 'It\'s a tie!'
+    }
+    
+    if(user_choice === 'paper') {
+        if(computer_choice === 'rock') {
+            return 'You win! Paper beats Rock'
+        }
+
+        if(computer_choice === 'scissor') {
+            return 'You lose! Scissor beats Paper'
+        }
+    }
+    
+    if(user_choice === 'rock') {
+        if(computer_choice === 'scissor') {
+            return 'You win! Rock beats Scissor'
+        }
+
+        if(computer_choice === 'paper') {
+            return 'You lose! Paper beats Rock'
+        }
+    }
+    
+    if(user_choice === 'scissor') {
+        if(computer_choice === 'paper') {
+            return 'You win! Scissor beats Paper'
+        }
+
+        if(computer_choice === 'rock') {
+            return 'You lose! Rock beats Scissor'
+        }
+    }
+}
